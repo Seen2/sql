c87d46c15a2c26d83d0f429bf20307dd8835933d
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-07-29 01:02:15 +0530

    Update 0-createBasic.sql

--- 0-createBasic.sql
+++ 0-createBasic.sql
@@ -7,8 +7,14 @@ CREATE TABLE flights(
 /* INSERT INTO flights VALUES(1,'IN','US',12);
 INSERT INTO flights VALUES(2,'IN','NZ',12);
 INSERT INTO flights VALUES(3,'US','NZ',12);
+varchar for large datasize and char for small
+integer for whole number
+number allows to restrict the length or size of integer data
+for deciamal number NUMBER(percision=5,scale=2)--->  _ _ _ . _ _  
+    default goes to max limit
 */
 
 SELECT * FROM flights;
 SELECT Length(origin),Length(destination) FROM flights;
+DESC flights;
 
