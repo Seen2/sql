1162dbc2a33470d6b4053cb452157522e91e0c75
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-07-29 03:25:45 +0530

    Update 7-selfRefrencialForeignKey.sql

--- 7-selfRefrencialForeignKey.sql
+++ 7-selfRefrencialForeignKey.sql
@@ -1,7 +1,7 @@
 CREATE TABLE students(
 stId INTEGER PRIMARY KEY,
 name VARCHAR(20),
-mentID INTEGER REFERENCES students(stId) /* self referenced*/
+mentID INTEGER REFERENCES students(stId) /* self referenced but take care that referenced var exist or not*/
 );
 INSERT INTO students(stId,name) VALUES(1,'Alex');
 INSERT INTO students VALUES(2,'MacGyver',1);
