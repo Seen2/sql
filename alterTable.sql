8b0f936df1ae49912562917124154e344328a219
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-07-29 23:38:18 +0530

    Create alterTable.sql

---
+++ alterTable.sql
@@ -0,0 +1,23 @@
+CREATE TABLE students(
+    name VARCHAR(20),
+    roll INTEGER
+);
+
+INSERT INTO students VALUES('Shintu',9);
+
+SELECT * FROM students;
+DESC students;
+
+ALTER TABLE students ADD stdId INTEGER ;
+/*for add and delete to alter it with more than one column use parenthesis: ADD(stdid INTEGER,intro VARCHAR(150))*/
+UPDATE students SET stdId=009 WHERE roll=9;
+ALTER TABLE students DROP(roll) ;
+
+/*
+to change data type(only if column is empty) or size of var we can also use ALTER .
+*/
+ALTER TABLE students ADD roll INTEGER ;
+ALTER TABLE students MODIFY roll NUMBER(3) ;
+ALTER TABLE students RENAME COLUMN stdid TO sid;
+
+drop table students
