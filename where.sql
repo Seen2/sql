b5174192cba696703acd2487eb0439b0e531da6d
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-07-30 21:20:47 +0530

    Create where.sql

---
+++ where.sql
@@ -0,0 +1,19 @@
+CREATE TABLE flights(
+    id INTEGER  PRIMARY KEY,
+    origin VARCHAR2(20) NOT NULL,
+    destination VARCHAR2(20) NOT NULL,
+    duration INTEGER NOT NULL
+);
+INSERT INTO flights VALUES(1,'IN','US',12);
+INSERT INTO flights VALUES(2,'IN','NZ',12);
+INSERT INTO flights VALUES(3,'US','NZ',12);
+
+select * from flights;
+
+/*
+data are case sensative but commands.
+trailing space get removed while comparison but leading dont.
+*/
+select * from flights where id=3;
+select id,origin Departure,destination as "Destination" from flights where id=3;
+select id,origin Departure,destination as "Destination" ,duration  from flights where origin='IN' and duration=12;
