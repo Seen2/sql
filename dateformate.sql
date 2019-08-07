d352aceaf87ade71108524773cea71558c5ed15b
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-08-08 01:10:20 +0530

    Update dateformate.sql

--- dateformate.sql
+++ dateformate.sql
@@ -2,7 +2,7 @@ select to_char(sysdate) as unformateddate from dual;
 /*Indian format*/
 select to_char(sysdate,'dd/mm/yy') as formateddate from dual;
 /*american format*/
-select to_char(sysdate,'mm/dd/yy') as unformateddate from dual;
+select to_char(sysdate,'mm/dd/yy') as formateddate from dual;
 
 /*
 Dy    -> Abbreviated name of the day -> Wed
@@ -16,3 +16,7 @@ YYYY  -> Four digit                  -> 2020
 YY    -> Two digit                   -> 20
 
 */
+select to_char(sysdate,'Day DD Month YYYY') as formateddate from dual;
+select to_char(sysdate,'Dy DD Mon YYYY') as formateddate from dual;
+select to_char(sysdate,'Day') as formateddate from dual;
+select to_char(sysdate,'Dy') as formateddate from dual;
