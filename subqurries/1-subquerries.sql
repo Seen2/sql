66cd805beff38a1a1e667a14826ef317f265cd71
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-08-13 10:21:23 +0530

    Update 1-subquerries.sql

--- subqurries/1-subquerries.sql
+++ subqurries/1-subquerries.sql
@@ -30,6 +30,17 @@ insert into passengers values(1,'MacGyver',2);
 insert into passengers values(2,'Jack',2);
 insert into passengers values(3,'Riley',9);
 insert into passengers values(4,'Bozer',9);
-
+insert into passengers values(5,'MacGyver',9);
+insert into passengers values(6,'Jack',3);
+insert into passengers values(7,'Riley',3);
+insert into passengers values(8,'MacGyver',3);
+insert into passengers values(9,'Bozer',4);
 select journey, duration from 
 (select f.origin || ' - ' || f.destination as journey, duration  from flights f order by duration asc) e where rownum <4;
+
+select s.name, count(s.courseid) from students s group by s.name;
+
+/*inline subqurries*/
+select fid , count(pid) freq from
+(select f.id as fid,p.flightid as pid from flights f left outer join passengers p on f.id=p.flightid) 
+group by fid order by count(pid) desc ;
