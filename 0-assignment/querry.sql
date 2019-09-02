3f441928101675ea27b9edb7868dc3e08556f0aa
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2019-09-02 13:31:36 +0530

    Update querry.sql

--- 0-assignment/querry.sql
+++ 0-assignment/querry.sql
@@ -23,6 +23,17 @@ update product_master set sell_price=950.00 where  description='1.22 Floppies';
 --change city of salesman to Mumbai
 update salesman_master set city='Mumbai' where  city='Bombay';
 
+--05. deleting 
+--delet slaseman where sal=3000
+delete from salesman_master where sal_amt=3000;
+--delete product whose quantity on hand equal to 100
+delete from product_master where qt_on_hand=100;
+--delete clients wher state is tamil nadu
+delete from client_master where state='Tamil Nadu';
+
+--06. Alter
+
+
 
 
 
