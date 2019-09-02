04915b441c7d5ee5e5beeecaefa9334908960369
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2019-09-02 12:10:55 +0530

    Create insert.sql

---
+++ 0-assignment/insert.sql
@@ -0,0 +1,36 @@
+insert into client_master(client_no,name,city,pincode,state,bal_due) values('C00001','Ivan Bayross','Bombay',400054,'Maharastra',15000);
+insert into client_master(client_no,name,city,pincode,state,bal_due) values('C00002','Vandana Saitwal','Madras',780001,'Tamil Nadu',0);
+insert into client_master(client_no,name,city,pincode,state,bal_due) values('C00003','Pramada Jaguste','Bombay',400057,'Maharastra',5000);
+insert into client_master(client_no,name,city,pincode,state,bal_due) values('C00004','Basu Navindgi','Bombay',400056,'Maharastra',0);
+insert into client_master(client_no,name,city,pincode,bal_due) values('C00005','Ravi Sreedharan','Delhi',100001,2000);
+insert into client_master(client_no,name,city,pincode,state,bal_due) values('C00006','Rukmini','Bombay',400050,'Maharastra',0);
+
+
+
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00001', '1.44 Floppies',5,'Piece',100,20,525,500);
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00002', 'Monitors',6,'Piece',10,3,12000,11280);
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00003', 'Mouse',5,'Piece',20,5,1050,1000);
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00004', '1.22 Floppies',5,'Piece',100,20,525,500);
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00005', 'Keyboards',2,'Piece',10,3,31050,3050);
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00006', 'CD Drive',2.5,'Piece',10,3,8400,8000);
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00007', '5.44 HDD',4,'Piece',10,3,1050,1000);
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00008', '1.44 Drive',5,'Piece',10,3,1050,1000);
+insert into product_master(product_no,description,profit_percent,unit_measure,qty_on_hand,record_lvl,sell_price,cost_price) values('P00009', '1.22 Drive',5,'Piece',2,3,1050,1000);
+
+
+
+
+insert into salesman_master(salesman_no,salesman_name,addres1,addres2,city,pincode,state,sal_amt,tgt_to_get,ytd_to_sales,remarks) values('S00001','Kiran','A/14','Worli','Bombay',400002,'Maharastra',3000,100,50,'Good');
+
+insert into salesman_master(salesman_no,salesman_name,addres1,addres2,city,pincode,state,sal_amt,tgt_to_get,ytd_to_sales,remarks) values('S00002','Manish','65','Nairman','Bombay',400001,'Maharastra',3000,200,100,'Good');
+
+
+insert into salesman_master(salesman_no,salesman_name,addres1,addres2,city,pincode,state,sal_amt,tgt_to_get,ytd_to_sales,remarks) values('S00003','Ravi','P-7','Bandra','Bombay',4000032,'Maharastra',3000,200,100,'Good');
+insert into salesman_master(salesman_no,salesman_name,addres1,addres2,city,pincode,state,sal_amt,tgt_to_get,ytd_to_sales,remarks) values('S00004','Ashish','A/5','Juhu','Bombay',4000044,'Maharastra',3500,200,150,'Good');
+
+
+
+
+
+
+
