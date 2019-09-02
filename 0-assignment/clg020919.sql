c750244fca25a2fd36757932aa4e40762084f759
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2019-09-02 15:06:55 +0530

    Update clg020919.sql

--- 0-assignment/clg020919.sql
+++ 0-assignment/clg020919.sql
@@ -13,3 +13,36 @@ insert into std_42 values(6,'XY',2);
 --select name, dept having dept count is grater than 1
 
 select name ,dept from std_42 where dept in (select dept from std_42 group by dept having count(dept)>1);
+
+
+ -- inbuilt func
+  --  to_char,to_date,substr,soundex,avg,min,max,power,abs,count,sum,round
+  
+--String
+      --length
+      select length('chandrayan') from dual;
+      -->10
+
+      --SUBSTR
+      select substr('chandrayan',3,5) from dual;
+      --> andra
+      select substr('chandrayan',-1,-5) from dual;
+      --> ''
+
+      -- trim
+      select ltrim('nnchandrayann','n') from dual;
+      -->chandrayannn
+      select rtrim('nnchandrayann','n') from dual;
+      -->nnchandraya
+
+      --padding func
+      select rpad('chandrayan',12,'$') from dual;
+      -->chandrayan$$
+      select rpad('chandrayan',7,'$') from dual;
+      -->chandrayan
+      select lpad('chandrayan',12,'$') from dual;
+      -->$$andrayan
+      select lpad('chandrayan',7,'$') from dual;
+      -->chandra
+
+
