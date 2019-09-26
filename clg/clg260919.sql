6b33d11ea46b329830a1ebb67e8f7d6ab77b55a7
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2019-09-26 10:53:40 +0530

    Create clg260919.sql

---
+++ clg/clg260919.sql
@@ -0,0 +1,23 @@
+set serveroutput on;
+
+declare
+
+begin
+  for i in 1..5 loop
+    for j in 1..i loop
+      dbms_output.put(' '||j);
+    end loop;
+    dbms_output.put_line('');
+  end loop;
+end;
+
+/*
+OUTPUT:
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
