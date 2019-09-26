702a731d6f7ef54ebe8203b5efa5676e9b9219da
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2019-09-26 11:22:18 +0530

    Update clg260919.sql

--- clg/clg260919.sql
+++ clg/clg260919.sql
@@ -39,4 +39,32 @@ new 2: n number:=8;
 1 2 3 4 5 6 7 8
 
 */
+--for multiple execution
 
+set serveroutput on;
+
+declare
+
+begin
+  for i in 1..5 loop
+    for j in 1..i loop
+      dbms_output.put(j||' ');
+    end loop;
+    dbms_output.put_line('');
+  end loop;
+end;
+/
+set serveroutput on;
+
+declare
+  n number:=&n;
+
+begin
+  
+  for i in 1..n loop
+    for j in 1..i loop
+      dbms_output.put(j||' ');
+    end loop;
+    dbms_output.put_line('');
+  end loop;
+end;
